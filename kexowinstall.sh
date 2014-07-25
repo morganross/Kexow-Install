8fad445cfdaf29fe19fbc624caa51568b429d9e6
morganross <morganross@rossmorr.com>
2014-07-25 13:19:16 -0700

    Create kexowinstall.sh

---
+++ kexowinstall.sh
@@ -0,0 +1,17 @@
+bin bash
+
+
+install git
+the readme for this file is going to replace "total build list"
+
+Are you sure this is ubuntu 12.04
+Do you want to install server or client?
+Do you want to change the ami name?
+Do you want to download security files or enter them manually now?
+Where would you like to download the secuirty files from.
+
+
+ask for secrete tar website
+download security files
+download Kexow-setup-scripts and kexow-website
+install em
