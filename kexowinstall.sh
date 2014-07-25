4b869174cd10be406cf4a3f89c675eef55974d1f
morganross <morganross@rossmorr.com>
2014-07-25 16:55:44 -0700

    update

--- kexowinstall.sh
+++ kexowinstall.sh
@@ -4,11 +4,14 @@ bin bash
 install git
 the readme for this file is going to replace "total build list"
 
-Are you sure this is ubuntu 12.04
-Do you want to install server or client?
-Do you want to change the ami name?
+#Are you sure this is ubuntu 12.04
+#Do you want to install server or client? fuck the client for now
+#Do you want to change the ami name?
 Do you want to download security files or enter them manually now?
+
 Where would you like to download the secuirty files from.
+then copy those files to home dir
+and run main menu
 
 
 ask for secrete tar website
@@ -19,3 +22,7 @@ install em
 
 
 this is a test of notepadd++ but not really
+
+put lamp.sh and ldap.sh into the main menu.
+put serer_script into main meu
+
