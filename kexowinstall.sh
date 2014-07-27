18f67b185a2a1e579a46b393a977f4829c7bfbfa
Morgan Ross Egging <morganross@rossmorr.com>
2014-07-26 22:48:39 -0700

    Update kexowinstall.sh

--- kexowinstall.sh
+++ kexowinstall.sh
@@ -6,14 +6,15 @@ download Kexow-setup-scripts
 
 where u want to download security file tar from
 
-extract tar
+extract creds.tar
+##cred.tar should cotain 4 files. pk creds cert and client. here is an explaination of their aws equivelints and how to generate them, or mb this is inthe readme
 
 run main menu
 
 ##include lamp.sh into main menu
 ##include server script in main menu
 
-
+enter in password, then use it to change pass in dbsettings and changestatus
 
 
 
