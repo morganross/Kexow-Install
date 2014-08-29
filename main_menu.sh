66bb6471e60bf42c92359e747eaf2a8b2316ce2e
morganross <morganross@rossmorr.com>
2014-08-28 23:50:32 -0700

    a

--- main_menu.sh
+++ main_menu.sh
@@ -334,7 +334,7 @@ sudo apt-get -y install nslcd
 
 #sudo sed -i "s/dc=example,dc=com/${ldap_base}/g" /etc/phpldapadmin/config.php
 
-NEW CHANGES111fdrt
+NEW CHANGES111fdrtffff
 
 
 sudo apt-get -y  install nfs-kernel-server
