b5803b9499c27ac9f3d33e1748ddd0278517f50a
morganross <morganross@rossmorr.com>
2014-08-28 23:07:28 -0700

    f4

--- main_menu.sh
+++ main_menu.sh
@@ -327,7 +327,7 @@ sudo echo 'libnss-ldap     shared/ldapns/ldap-server       string   ldapi://54.2
 sudo echo 'libnss-ldap     libnss-ldap/nsswitch    note' | sudo debconf-set-selections -v
 sudo echo 'libnss-ldap     libnss-ldap/confperm    boolean false' | sudo debconf-set-selections -v
 sudo echo 'libnss-ldap     libnss-ldap/dbrootlogin boolean true' | sudo debconf-set-selections -v
-#change change2 change3
+#change change2 change3 change 4
 
 sudo apt-get -y install libnss-ldap
 sudo apt-get -y install libpam-ldap
